5a4294edc0ee79e028749ec5a45c0ccb10a014a9
Johan Rosa <johan.rosaperez@gmail.com>
2020-07-10 21:14:11 -0400

    with modeling section

--- scripts/ejercicio_completo.R
+++ scripts/ejercicio_completo.R
@@ -1,23 +1,32 @@
 
 # Paquetes --- -------------------------------------------------------------
+# data manipulation
 library(tidyverse)
 library(lubridate)
 library(janitor)
 
+# time series and forecasting
+library(tsibble)
+library(fable)
+library(fabletools)
+library(feasts)
+
+library(scico)
+
 # Importar y preparar data --- ----------------------------------------------
 
 # Precios de los combustibles ----
-combustibles <- read_delim("data/datasemanal.txt", delim = '\t') %>%
+combustibles <- readxl::read_excel("data/semana_combustible_new_date.xlsx") %>% 
   janitor::clean_names() %>% 
-  mutate(fecha = mdy(fecha)) %>%  
-  filter(!is.na(premium)) 
+  mutate(fecha = as.Date(fecha)) %>% 
+  filter(!is.na(premium))
 
 # para filtrar en el siguiente paso
 last_date <- last(pull(combustibles, fecha))
 
 # secuencia de fechas desde marzo del 2001
 combustibles_diarios <- tibble(fecha = seq(from = ymd("2001-03-20"), to = Sys.Date(), by = "day")) %>% 
-  # se resta cuatro para llevar las fechas de día miercoles de cada semana,
+  # se resta cuatro para llevar las fechas de día miércoles de cada semana,
   # al sábado anterior
   left_join(mutate(combustibles, fecha = fecha - 4)) %>%
   fill(-fecha, .direction = "down") %>% 
@@ -32,7 +41,8 @@ combustibles_month <- group_by(combustibles_diarios, date_ym) %>%
 # Data con precios ajustados
 precios <- combustibles_month %>% 
   select(date_ym, premium, regular, glp, gasoil_reg) %>% 
-  pivot_longer(cols = -date_ym, names_to = "name", values_to = "precio") %>% 
+  pivot_longer(cols = -date_ym, names_to = "name", values_to = "precio") %>%
+  arrange(name, date_ym) %>% 
   mutate(
     name = recode(
       name, "premium" = "Gasolina premium",
@@ -49,64 +59,81 @@ source("scripts/get_ipc_articulos.R")
 # Descarga la data del ipc a todos lo niveles de desagregación posible
 ipc_desagregado <- get_ipc_articulos()
 
-# se ajusta la data para extraer solo los artículos, si agregaciones superirores
-ipc_articulos <- ipc_desagregado %>%
+# adecuando algunos campos para tener el árbol ascendente de cada artículo
+ipc_desagregado <- ipc_desagregado %>%
   mutate(
     date_ym = paste(year, crear_mes(mes, "text_to_number"), "01", sep = "-"),
     date_ym = tsibble::yearmonth(ymd(date_ym)),
-    grupo = ifelse(division == "Grupo", nombre, NA),
-    subgrupo = ifelse(division == 'Subgrupo', nombre, NA),
-    clase = ifelse(division == 'Clase', nombre, NA),
-    subclase = ifelse(division == "SubClase", nombre, NA)
+    grupo = ifelse(division == "Grupo", name, NA),
+    subgrupo = ifelse(division == 'Subgrupo', name, NA),
+    clase = ifelse(division == 'Clase', name, NA),
+    subclase = ifelse(division == "SubClase", name, NA)
   ) %>%
-  fill(c(grupo, subgrupo, clase, subclase), .direction = "down") %>% 
-  filter(division == "Articulo") 
-  
+  fill(c(grupo, subgrupo, clase, subclase), .direction = "down")
 
-indices <- ipc_articulos %>% 
-  filter(subclase == "Combustibles y lubricantes para vehículos") %>%
-  mutate(name = ifelse(str_detect(name, 'Gas lic'), "GLP", name)) %>%
-  filter(name != "Lubricantes y aceites para vehículos") %>% 
-  select(date_ym, name, ponderador, indice)
-  
+# series de los artículos que compnone la clase de combustibles y lubricantes
+ipc_combustibles <- ipc_desagregado %>% 
+  filter(clase == "Combustibles y lubricantes para equipo de transporte",
+         division %in% c("SubClase", "Articulo")) %>%
+  select(year, mes, date_ym, name, division, ponderador, indice) %>% 
+  mutate(name = recode(name, "Gas licuado para vehículos (GLP)" = "GLP")) %>% 
+  arrange(name, date_ym)
 
-# Unión precios indices ---------------------------------------------------
-
-indice_precio <- indices %>% 
-  left_join(precios) %>%
-  arrange(name, date_ym) %>%
+# agregando la incidencia y las variaciones mensuales e interanuales
+ipc_combustibles <- ipc_combustibles %>% 
   group_by(name) %>% 
-  mutate(indice_vm = (indice / lag(indice, 1) - 1),
-         precio_vm = (precio / lag(precio, 1) - 1))
+  mutate(
+    variacion_mes = indice / lag(indice) - 1,
+    variacion_interanual = indice / lag(indice, 12) - 1,
+    #to_see = mean(ponderador[division == "SubClase"]),
+    incidencia = ponderador / mean(ponderador[division == "SubClase"])
+  ) %>% ungroup()
+
+# merge de los precios y los índices de los precios de los combustibles
+combustibles_precio_indice <- left_join(
+    precios, ipc_combustibles,
+    by = c("date_ym", "name")
+  ) %>% 
+  select(year, mes, date_ym, name, division, ponderador, indice, precio) %>% 
+  filter(year > 2010)
 
 
+# Modeling --- -------------------------------------------------------------
+  
+# Anidando la data
+combustibles_nested <- ipc_combustibles %>%
+  group_by(name, division, ponderador) %>% 
+  nest() 
 
-# Visualizaciones ---------------------------------------------------------
-
-# Evolución variación mensual por combustibles
-indice_precio %>% 
-  ggplot() +
-  geom_line(aes(x = date_ym, y = indice_vm, color = "Índice") ) +
-  geom_line(aes(x = date_ym, y = precio_vm, color = "Precio")) +
-  facet_wrap(~name) +
-  theme_light() +
-  scale_y_continuous(labels = scales::percent) +
-  scale_x_yearmonth(breaks = scales::pretty_breaks(8)) +
-  theme(legend.position = "bottom",
-        panel.grid = element_blank()) +
-  scale_color_manual(values = c("black", " gray69")) +
-  labs(y = "Variación mensual",
-       x = NULL, color = NULL)
-
-
-# histomgrama de los precios 
-
-indice_precio %>% 
-  ggplot(aes(x = precio_vm)) +
-  geom_histogram(binwidth = 0.02) +
-  scale_x_continuous(labels = scales::percent, breaks = scales::pretty_breaks(7)) +
-  theme_light() +
-  labs(x = NULL, y = "Frecuencia") +
-  facet_wrap(~name, scales = "free_x")
-
-
+combustibles_nested %>% 
+  mutate(
+    # serie de tiempo
+    ts = map(
+      data,
+      ~.x %>% select(variacion_mes) %>% 
+        slice(-1) %>% 
+        ts(start = c(2011, 2), frequency = 12)
+      ),
+    
+    # Parametros para los modelos
+    params = map(
+      ts,
+      ~list(
+        auto.arima = list(y = .x),
+        ets = list(y = .x),
+        bats = list(y = .x),
+        thetaf = list(y = .x)
+        )
+    ),
+    
+    params = map(params, enframe, name = "f", value = "params"),
+    
+    # Fiting models
+    models = map(
+      params,
+      ~.x %>% mutate(fit = invoke_map(f, params))
+    ),
+    
+    # Foreacast
+    
+  )
